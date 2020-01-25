15c8c3a31a6e1b6dbd23d295d818d4d6e8096fa7
avalencia05 <alejandra.valencia05@outlook.com>
2020-01-24 18:31:10 -0800

    v4

--- hw4.js
+++ hw4.js
@@ -1,37 +1,37 @@
 //Define variables
 const
-    FirstBBtn = document.querySelector("#FirstB"),
-	SecondBBtn = document.querySelector("#SecondB");
+    startBtn = document.querySelector("#FirstB"),
+	changeBtn = document.querySelector("#SecondB");
 	
 //Let variable array 
-let MyArray = [];
+let wordArray = [];
  
 
 // Function that tells the button what to do 
-FirstBBtn.addEventListener("click",myFirstFunction,true);
-SecondBBtn.addEventListener("click",secondChangeWord,true);
-SecondBBtn.style.display = "none";
+startBtn.addEventListener("click",myFunction,true);
+changeBtn.addEventListener("click",secondChangeWord,true);
+changeBtn.style.display = "none";
 
 
 
-function myFirstFunction(){
+function myFunction(){
 // collecting
-	for(let MyIndex = 0; MyIndex < 3; MyIndex++ ){
-		MyArray.push(prompt('Please enter a word'));
+	for(let i = 0; i < 3; i++ ){
+		wordArray.push(prompt('Please enter a word'));
 	}
 
 	{	
  //displaying 
 		let out = "<ul>"
-		for(let MyIndex = 0;  MyIndex <3;  MyIndex ++){
-			out = out + "<li>" + MyArray[MyIndex] + "</li>"
+		for(let i = 0;  i <3;  i ++){
+			out = out + "<li>" + wordArray[i] + "</li>"
 
 		}
 		out = out + "</ul>"
 		document.getElementById("words").innerHTML = out
-		SecondBBtn.style.display = "block";
-    FirstBBtn.style.display = "none";
-    FirstPage.style.display = "show";
+		changeBtn.style.display = "block";
+    startBtn.style.display = "none";
+    startBtn.style.display = "show";
 SecondPage.style.display = "none";
 	}
 	
